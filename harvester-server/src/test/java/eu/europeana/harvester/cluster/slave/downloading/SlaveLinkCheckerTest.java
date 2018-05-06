f1f4e11c850f26a32cee506cbeeae3cc46954d7a
Dan Todor <dantodor@gmail.com>
2018-05-06 13:09:35 +0700

    Fix tests 5

--- harvester-server/src/test/java/eu/europeana/harvester/cluster/slave/downloading/SlaveLinkCheckerTest.java
+++ harvester-server/src/test/java/eu/europeana/harvester/cluster/slave/downloading/SlaveLinkCheckerTest.java
@@ -1,17 +1,25 @@
 package eu.europeana.harvester.cluster.slave.downloading;
 
 import eu.europeana.harvester.cluster.domain.messages.RetrieveUrl;
+import eu.europeana.harvester.cluster.slave.HttpServer;
 import eu.europeana.harvester.domain.*;
 import eu.europeana.harvester.httpclient.response.HttpRetrieveResponse;
 import eu.europeana.harvester.httpclient.response.HttpRetrieveResponseFactory;
 import eu.europeana.harvester.httpclient.response.RetrievingState;
 import eu.europeana.harvester.httpclient.response.ResponseType;
 import org.apache.logging.log4j.LogManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 
+import static eu.europeana.harvester.TestUtils.PATH_DOWNLOADED;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -25,9 +33,31 @@ public class SlaveLinkCheckerTest {
 
     private static org.apache.logging.log4j.Logger LOG = LogManager.getLogger(SlaveDownloaderTest.class.getName());
     private static final String text1GitHubUrl = TestUtils.GitHubUrl_PREFIX + TestUtils.Image1;
-
+    private final static int port = 9090;
+    private static HttpServer server;
+    private static final String assetDir = "./assets";
     final HttpRetrieveResponseFactory httpRetrieveResponseFactory = new HttpRetrieveResponseFactory();
 
+
+    @Before
+    public void setUp()  {
+        try {
+            server = new HttpServer(port);
+            HttpServer.VirtualHost host = server.getVirtualHost(null); // default host
+            host.setAllowGeneratedIndex(true); // with directory index pages
+            File dir = new File(assetDir);
+            host.addContext("/", new HttpServer.FileContextHandler(dir));
+            server.start();
+            System.out.println("HttpServer is listening on port " + port);
+        } catch (Exception e) {
+            System.err.println("error: " + e);
+        }
+    }
+
+    @After
+    public void tearDown() {
+        server.stop();
+    }
     @Rule
     public TestRule watcher = new TestWatcher() {
         protected void starting(Description description) {
@@ -49,9 +79,9 @@ public class SlaveLinkCheckerTest {
         assertEquals(RetrievingState.COMPLETED, response.getState());
 
         assertNotNull(response.getSourceIp());
-        assertTrue(response.getSocketConnectToDownloadStartDurationInMilliSecs() > 5);
-        assertTrue(response.getCheckingDurationInMilliSecs() > 50);
-        assertTrue(response.getRetrievalDurationInMilliSecs() > 50);
+        assertTrue(response.getSocketConnectToDownloadStartDurationInMilliSecs() > 0);
+        assertTrue(response.getCheckingDurationInMilliSecs() > 0);
+        assertTrue(response.getRetrievalDurationInMilliSecs() > 0);
     }
 
     @Test
@@ -75,8 +105,8 @@ public class SlaveLinkCheckerTest {
         assertEquals(RetrievingState.FINISHED_TIME_LIMIT, response.getState());
 
         assertNotNull(response.getSourceIp());
-        assertTrue(response.getSocketConnectToDownloadStartDurationInMilliSecs() > 5);
-        assertTrue(response.getCheckingDurationInMilliSecs() > 50);
+        assertTrue(response.getSocketConnectToDownloadStartDurationInMilliSecs() > 0);
+        assertTrue(response.getCheckingDurationInMilliSecs() > 0);
         assertEquals(response.getRetrievalDurationInMilliSecs().longValue(), 0);
     }
 
@@ -95,9 +125,9 @@ public class SlaveLinkCheckerTest {
         assertEquals(404, response.getHttpResponseCode().longValue());
 
         assertNotNull(response.getSourceIp());
-        assertTrue(response.getSocketConnectToDownloadStartDurationInMilliSecs() > 5);
-        assertTrue(response.getCheckingDurationInMilliSecs() > 50);
-        assertTrue(response.getRetrievalDurationInMilliSecs() > 50);
+        assertTrue(response.getSocketConnectToDownloadStartDurationInMilliSecs() > 0);
+        assertTrue(response.getCheckingDurationInMilliSecs() > 0);
+        assertTrue(response.getRetrievalDurationInMilliSecs() > 0);
     }
 
 
